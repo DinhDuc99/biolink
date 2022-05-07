e1bfce8bb6887ca84181426034a1a381cdfaf2a1
DinhDuc99 <dinhduc0406@gmail.com>
2022-05-07 21:55:54 +0700

    đeploy

---
+++ pages/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class = 'container'>
+    <div class ="detail">
+      <img class="detail-img" src="https://cdn.bio.link/uploads/profile_pictures/2022-05-07/s65AvV4QoEHyGyZU98usmb6XWikIyUto.png" alt="">
+      <h3>Đình Đức</h3>
+      <div class="icons">
+        <div class="icon">
+          <a class="social-icon-anchor" data-id= "instagram" href="https://instagram.com/nguyendinhduc.thai"><svg class="social-icon-fill" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.25 9C0.25 4.16751 4.16751 0.25 9 0.25H21C25.8325 0.25 29.75 4.16751 29.75 9V21C29.75 25.8325 25.8325 29.75 21 29.75H9C4.16751 29.75 0.25 25.8325 0.25 21V9ZM9 1.75C4.99594 1.75 1.75 4.99594 1.75 9V21C1.75 25.0041 4.99594 28.25 9 28.25H21C25.0041 28.25 28.25 25.0041 28.25 21V9C28.25 4.99594 25.0041 1.75 21 1.75H9ZM24 7.75H22V6.25H24V7.75ZM8.25 15C8.25 11.2721 11.2721 8.25 15 8.25C18.7279 8.25 21.75 11.2721 21.75 15C21.75 18.7279 18.7279 21.75 15 21.75C11.2721 21.75 8.25 18.7279 8.25 15ZM15 9.75C12.1005 9.75 9.75 12.1005 9.75 15C9.75 17.8995 12.1005 20.25 15 20.25C17.8995 20.25 20.25 17.8995 20.25 15C20.25 12.1005 17.8995 9.75 15 9.75Z" fill="white"></path></svg></a>
+        </div>
+        <div class="icon">
+          <a class="social-icon-anchor" data-id="twitter" href="https://twitter.com/dinhduc0406"><svg class="social-icon-fill" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M29.2021 2.29128C29.4871 2.3899 29.6853 2.64956 29.7052 2.95041C29.8142 4.59396 29.734 5.81882 29.3639 6.86221C29.0225 7.8245 28.4577 8.56742 27.7099 9.31547V10.375C27.7099 19.9699 19.9435 27.75 10.3606 27.75L10.2857 27.75C7.10613 27.7502 3.76846 27.7503 0.583588 25.6238C0.308808 25.4403 0.186459 25.0986 0.282317 24.7824C0.378175 24.4662 0.669657 24.25 1.00006 24.25C4.6982 24.25 7.82719 22.5613 9.52731 21.2664C5.55292 19.6867 2.78453 17.2448 1.43666 14.1767C-0.0476899 10.7979 0.256627 6.81007 2.32594 2.66503C2.45925 2.39801 2.7382 2.23543 3.03624 2.25106C3.33429 2.26669 3.5947 2.45754 3.69935 2.73704C4.59654 5.13323 7.9449 9.87584 14.2285 10.2291L14.2285 9.08222C14.2285 5.31 17.282 2.25003 21.0513 2.25003C22.8547 2.25003 24.4912 2.96875 25.6893 4.13362C26.6713 3.90172 27.6414 3.3409 28.3993 2.49842C28.6009 2.27427 28.9172 2.19266 29.2021 2.29128ZM26.6834 8.22046C27.3244 7.57355 27.715 7.02367 27.9502 6.36072C28.1163 5.89237 28.2154 5.33778 28.2424 4.61274C27.4133 5.17664 26.485 5.5459 25.5445 5.68833L25.1461 5.74867L24.8759 5.4498C23.9308 4.4046 22.5677 3.75003 21.0513 3.75003C18.1126 3.75003 15.7285 6.13624 15.7285 9.08222L15.7285 11.75H14.9785C8.56761 11.75 4.7119 7.74457 3.01957 4.78232C1.71748 8.06916 1.7083 11.0657 2.80998 13.5734C4.07399 16.4506 6.86624 18.8344 11.2221 20.2886C11.4685 20.3709 11.6548 20.5748 11.7145 20.8276C11.7743 21.0804 11.6989 21.3461 11.5154 21.5299C10.3491 22.698 7.43534 24.7769 3.65251 25.4965C5.76005 26.2408 7.96828 26.25 10.3606 26.25C19.1128 26.25 26.2099 19.1436 26.2099 10.375V8.91784C26.2099 8.60128 26.406 8.33053 26.6834 8.22046Z" fill="white"></path></svg></a>
+        </div>
+        <div class="icon">
+          <a class="social-icon-anchor" data-id="facebook" href="https://www.facebook.com/nguyendinhduc.thai"> <svg class="social-icon-fill" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.25 15C0.25 6.8538 6.8538 0.25 15 0.25C23.1462 0.25 29.75 6.8538 29.75 15C29.75 23.1462 23.1462 29.75 15 29.75C6.8538 29.75 0.25 23.1462 0.25 15ZM15 1.75C7.68223 1.75 1.75 7.68223 1.75 15C1.75 22.0661 7.28116 27.8403 14.25 28.2291V17.75H10V16.25H14.25V13C14.25 10.3766 16.3766 8.25 19 8.25H20V9.75H19C17.2051 9.75 15.75 11.2051 15.75 13V16.25H20V17.75H15.75V28.2291C22.7188 27.8403 28.25 22.0661 28.25 15C28.25 7.68223 22.3178 1.75 15 1.75Z" fill="white"></path></svg></a>       
+        </div>
+        <div class="icon">
+          <a class="social-icon-anchor" data-id="gmail.com" href="mailto:dinhduc0406@gmail.com"><svg class="social-icon-fill" width="30" height="30" viewBox="0 0 30 30" fill="black" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 3.25C1.48122 3.25 0.25 4.48121 0.25 6V24.4C0.25 25.9188 1.48122 27.15 3 27.15H27C28.5188 27.15 29.75 25.9188 29.75 24.4V6C29.75 4.48122 28.5188 3.25 27 3.25H3ZM1.75 6C1.75 5.30964 2.30964 4.75 3 4.75H27C27.6904 4.75 28.25 5.30964 28.25 6V24.4C28.25 25.0904 27.6904 25.65 27 25.65H3C2.30964 25.65 1.75 25.0904 1.75 24.4V6ZM24.4101 10.2003L15.4501 16.9203C15.1834 17.1203 14.8167 17.1203 14.5501 16.9203L5.59008 10.2003C5.25871 9.95178 5.19156 9.48168 5.44008 9.15031C5.68861 8.81894 6.15871 8.75178 6.49008 9.00031L15.0001 15.3828L23.5101 9.00031C23.8415 8.75178 24.3116 8.81894 24.5601 9.15031C24.8086 9.48168 24.7415 9.95178 24.4101 10.2003Z" fill="white"></path></svg></a>          
+        </div>
+      </div>
+      <div class="contents-detail">
+        <div class ='content-img'>
+          <img src="https://cdn.bio.link/uploads/thumbnails/2022-05-07/prSphv5SP04wlPrQploaded_R1VWf4Ou.gif" alt="">
+        </div>
+        <a class ='content' href="https://www.facebook.com/nguyendinhduc.thai">Hello Doof</a>
+      </div>
+      <div class="contents-detail">
+        <div class ='content-img'>
+          <img src="https://cdn.bio.link/uploads/thumbnails/2022-05-07/iFb5W5e6STmS87zDploaded_RNXiNfhi.gif" alt="" style="margin-top:4px">
+        </div>
+        <a class ='content' href="https://www.facebook.com/nguyendinhduc.thai">Hi EveryOne</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+*, *:before, *:after {
+    padding: 0;
+    margin: 0;
+    box-sizing: inherit;
+}
+html{
+    font-family: "Poppins",sans-serif;
+    text-transform: none;
+    box-sizing: border-box;
+    min-height: 100%;
+}
+body{
+  background: url(https://cdn.bio.link/themes/backgrounds/glassmorphism.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.container{
+  width: 100%;
+  
+}  
+.comname{
+  margin: auto;
+}
+
+.details{
+  margin-top: 50px;
+  align-items: center;
+}
+.detail-img{
+  width: 120px;
+  display: block;
+  height: 120px;
+  margin: auto;
+  margin-top: 60px;
+
+}
+h3{
+  text-align: center;
+  margin-top: 20px;
+  padding-left: 4px;
+}
+.icons{
+  display: block;
+  text-align: center;
+
+}
+.icon{
+  display: inline-block;
+  
+  margin:10px ;
+}
+.icon :hover{
+  transform: scale(1.1) ;
+}
+.social-icon-fill .social-icon-anchor{
+  fill: #320A0C !important;
+}
+
+
+.contents-detail{
+  display: flex ;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 20px;
+  height: 100%
+  
+}
+.contents-detail:hover{
+  transform: scale(1.03) ;
+}
+
+
+
+.content-img img{
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  top:0;
+}
+
+
+.content{
+  background-color: antiquewhite;
+  border-radius: 30px;
+  border:1px antiquewhite solid ;
+  width: 680px;
+  text-decoration: none;
+  color: #320A0C;
+  text-align: center;
+  padding-top:20px;
+  height: 60px;
+  font-weight: 600;
+  font-size: 20px;
+  background: linear-gradient(263.81deg, rgba(255, 255, 255, 0.4) 18.8%, rgba(255, 255, 255, 0) 73.34%), rgba(255, 255, 255, 0.25);
+}
+.content:hover{
+  transform: scale(1.03) ;
+}
+ @keyframes rotateImg{
+   from{transform:rotate(0) scale(2.5)}
+   to{transform:rotate(5000deg) scale(2.5)}
+   }
+</style>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/biolink/' // Thay tên repository của các bạn vào đây nhé
+      : '/'
+  }
